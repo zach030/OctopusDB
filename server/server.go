94a6bd659a79b0f90ea93f15fa05b98d80f28f6d
zach <zachchou016@gmail.com>
2022-01-24 14:02:53 +0800

    add proto and server
    Signed-off-by: zach <zachchou016@gmail.com>

--- server/server.go
+++ server/server.go
@@ -5,3 +5,7 @@ import "github.com/zach030/OctopusDB/storage"
 type OctopusServer struct {
 	storage storage.Storage
 }
+
+func NewServer(storage storage.Storage) *OctopusServer {
+	return &OctopusServer{storage: storage}
+}
