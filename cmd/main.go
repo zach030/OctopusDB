fb5f05f19f1343a8abe92b4626d61ff9c008552f
zach <zachchou016@gmail.com>
2022-04-15 19:26:07 +0800

    fix octopus standalone kv
    Signed-off-by: zach <zachchou016@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,4 @@
 package main
 
-import (
-	"github.com/zach030/OctopusDB/cluster"
-	"github.com/zach030/OctopusDB/server"
-)
-
 func main() {
-	cluster.Start()
-	http := server.NewHttpServer()
-	go func() {
-		http.Start()
-	}()
 }
